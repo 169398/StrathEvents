4469376e21d6b0035996b09f3ce4b1dc84fb9ba7
idris kulubi <137810838+169398@users.noreply.github.com>
2024-03-27 09:16:17 +0300

    metadata

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -10,8 +10,8 @@ import { notFound } from 'next/navigation'
 import { ReactNode } from 'react'
 
 export const metadata: Metadata = {
-  title: 'absa connect',
-  description: 'absa social app',
+  title: 'Strath events',
+  description: 'Strathmore events application ',
 }
 
 const Layout = async ({
